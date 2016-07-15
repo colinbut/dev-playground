4b67777bb70c6dc715dea524e6ba154ea80cd9f4
Colin But <colinbut@users.noreply.github.com>
2016-07-15 22:00:38 +0100

    added first junit 5 test

--- junit-5/src/test/java/com/mycompany/junit5/FirstJUnit5Test.java
+++ junit-5/src/test/java/com/mycompany/junit5/FirstJUnit5Test.java
@@ -5,9 +5,21 @@
  */
 package com.mycompany.junit5;
 
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * @author colin
  */
+@RunWith(JUnitPlatform.class)
 public class FirstJUnit5Test {
+
+    @Test
+    public void firstTest() {
+        assertEquals(2, 1 + 1);
+    }
 
 }
