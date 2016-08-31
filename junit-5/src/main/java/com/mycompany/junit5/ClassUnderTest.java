c25c84bd609cc47761a2f6c82effdd9d4c092b9e
Colin But <colinbut@users.noreply.github.com>
2016-08-31 21:07:37 +0100

    deleted junit 5 sub module project

--- junit-5/src/main/java/com/mycompany/junit5/ClassUnderTest.java
+++
@@ -1,9 +0,0 @@
-/*
- * |-------------------------------------------------
- * | Copyright © 2016 Colin But. All rights reserved. 
- * |-------------------------------------------------
- */
-package com.mycompany.junit5;
-
-public class ClassUnderTest {
-}
